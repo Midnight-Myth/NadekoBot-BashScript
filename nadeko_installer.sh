ddad7e1c8b600197e994b618eaac784391a991f0
Midnight-Myth <Midnight-Myth@users.noreply.github.com>
2017-05-18 17:58:30 +0200

    Update nadeko_installer.sh

--- nadeko_installer.sh
+++ nadeko_installer.sh
@@ -37,21 +37,6 @@ echo ""
 echo "NadekoBot downloaded."
 
 echo ""
-echo "Downloading Nadeko dependencies"
-cd $root/$tempdir/NadekoBot/Discord.Net/src/Discord.Net.Core/
-dotnet restore 1>/dev/null 2>&1
-cd $root/$tempdir/NadekoBot/Discord.Net/src/Discord.Net.Rest/
-dotnet restore 1>/dev/null 2>&1
-cd $root/$tempdir/NadekoBot/Discord.Net/src/Discord.Net.WebSocket/
-dotnet restore 1>/dev/null 2>&1
-cd $root/$tempdir/NadekoBot/Discord.Net/src/Discord.Net.Commands/
-dotnet restore 1>/dev/null 2>&1
-cd $root/$tempdir/NadekoBot/src/NadekoBot/
-dotnet restore 1>/dev/null 2>&1
-echo ""
-echo "Download done"
-
-echo ""
 echo "Building NadekoBot"
 cd $root/$tempdir/NadekoBot/src/NadekoBot/
 dotnet build --configuration Release 1>/dev/null 2>&1
