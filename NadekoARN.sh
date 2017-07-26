8fb70f5b8a6988ae0e4042b4f504b3d9851c011f
Midnight-Myth <Midnight-Myth@users.noreply.github.com>
2017-07-26 11:17:42 +0200

    Update NadekoARN.sh

--- NadekoARN.sh
+++ NadekoARN.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 echo ""
-echo "Running NadekoBot with auto restart normally! (without updating)"
+echo "Running MidnightBot with auto restart normally! (without updating)"
 root=$(pwd)
 youtube-dl -U
 
 sleep 5s
-cd "$root/NadekoBot"
+cd "$root/MidnightBot"
 dotnet restore && dotnet build --configuration Release
 
-while :; do cd "$root/NadekoBot/src/NadekoBot" && dotnet run -c Release; sleep 5s; done
+while :; do cd "$root/MidnightBot/src/MidnightBot" && dotnet run -c Release; sleep 5s; done
 echo ""
 echo "That didn't work? Please report in #NadekoLog Discord Server."
 sleep 3s
 
 cd "$root"
-bash "$root/linuxAIO.sh"
+bash "$root/linuxAIOS.sh"
 echo "Done"
 
 exit 0
