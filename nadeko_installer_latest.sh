82d0e59814f74e9317ca446bc07478e7cb658836
Midnight-Myth <Midnight-Myth@users.noreply.github.com>
2017-07-26 11:27:15 +0200

    Update nadeko_installer_latest.sh

--- nadeko_installer_latest.sh
+++ nadeko_installer_latest.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo ""
-echo "NadekoBot Installer started."
+echo "MidnightBot Installer started."
 
 if hash git 1>/dev/null 2>&1
 then
@@ -24,49 +24,49 @@ else
 fi
 
 root=$(pwd)
-tempdir=NadekoInstall_Temp
+tempdir=MidnightInstall_Temp
 
 rm -r "$tempdir" 1>/dev/null 2>&1
 mkdir "$tempdir"
 cd "$tempdir"
 
 echo ""
-echo "Downloading NadekoBot, please wait."
-git clone -b 1.4 --recursive --depth 1 https://github.com/Kwoth/NadekoBot.git
+echo "Downloading MidnightBot, please wait."
+git clone -b master --recursive --depth 1 https://github.com/Midnight-Myth/Mitternacht-NEW.git
 echo ""
-echo "NadekoBot downloaded."
+echo "MidnightBot downloaded."
 
 echo ""
-echo "Downloading Nadeko dependencies"
-cd "$root/$tempdir/NadekoBot"
+echo "Downloading MidnightBot dependencies"
+cd "$root/$tempdir/MidnightBot"
 dotnet restore
 echo ""
 echo "Download done"
 
 echo ""
-echo "Building NadekoBot"
+echo "Building MidnightBot"
 dotnet build --configuration Release
 echo ""
-echo "Building done. Moving Nadeko"
+echo "Building done. Moving MidnightBot"
 
 cd "$root"
 
-if [ ! -d NadekoBot ]
+if [ ! -d MidnightBot ]
 then
-    mv "$tempdir"/NadekoBot NadekoBot
+    mv "$tempdir"/MidnightBot MidnightBot
 else
-    rm -rf NadekoBot_old 1>/dev/null 2>&1
-    mv -fT NadekoBot NadekoBot_old 1>/dev/null 2>&1
-    mv "$tempdir"/NadekoBot NadekoBot
-    cp -f "$root/NadekoBot_old/src/NadekoBot/credentials.json" "$root/NadekoBot/src/NadekoBot/credentials.json" 1>/dev/null 2>&1
+    rm -rf MidnightBot_old 1>/dev/null 2>&1
+    mv -fT MidnightBot MidnightBot_old 1>/dev/null 2>&1
+    mv "$tempdir"/MidnightBot MidnightBot
+    cp -f "$root/MidnightBot_old/src/MidnightBot/credentials.json" "$root/MidnightBot/src/MidnightBot/credentials.json" 1>/dev/null 2>&1
     echo ""
     echo "credentials.json copied to the new version"
-    cp -RT "$root/NadekoBot_old/src/NadekoBot/bin/" "$root/NadekoBot/src/NadekoBot/bin/" 1>/dev/null 2>&1
-    cp -RT "$root/NadekoBot/src/NadekoBot/bin/Release/netcoreapp1.0/data/NadekoBot.db" "$root/NadekoBot/src/NadekoBot/bin/Release/netcoreapp1.1/data/NadekoBot.db" 1>/dev/null 2>&1
-    mv -f "$root/NadekoBot/src/NadekoBot/bin/Release/netcoreapp1.0/data/NadekoBot.db" "$root/NadekoBot/src/NadekoBot/bin/Release/netcoreapp1.0/data/NadekoBot_old.db" 1>/dev/null 2>&1
+    cp -RT "$root/MidnightBot_old/src/MidnightBot/bin/" "$root/MidnightBot/src/MidnightBot/bin/" 1>/dev/null 2>&1
+    cp -RT "$root/MidnightBot/src/MidnightBot/bin/Release/netcoreapp1.0/data/NadekoBot.db" "$root/MidnightBot/src/MidnightBot/bin/Release/netcoreapp1.1/data/NadekoBot.db" 1>/dev/null 2>&1
+    mv -f "$root/MidnightBot/src/MidnightBot/bin/Release/netcoreapp1.0/data/NadekoBot.db" "$root/MidnightBot/src/MidnightBot/bin/Release/netcoreapp1.0/data/NadekoBot_old.db" 1>/dev/null 2>&1
     echo ""
     echo "Database copied to the new version"
-    cp -RT "$root/NadekoBot_old/src/NadekoBot/data/" "$root/NadekoBot/src/NadekoBot/data/" 1>/dev/null 2>&1
+    cp -RT "$root/MidnightBot_old/src/MidnightBot/data/" "$root/MidnightBot/src/MidnightBot/data/" 1>/dev/null 2>&1
     echo ""
     echo "Other data copied to the new version"
 fi
