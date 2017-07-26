63725437e3e35bb49f8a7e6774b7b7e3f07cb054
Midnight-Myth <Midnight-Myth@users.noreply.github.com>
2017-07-26 15:42:38 +0200

    Update NadekoARU_Latest.sh

--- NadekoARU_Latest.sh
+++ NadekoARU_Latest.sh
@@ -5,7 +5,7 @@ root=$(pwd)
 youtube-dl -U
 
 sleep 5s
-while :; do cd "$root/MidnightBot" && dotnet restore && dotnet build --configuration Release && cd "$root/MidnightBot/src/MidnightBot" && dotnet run -c Release && cd "$root" && wget -N https://github.com/Midnight-Myth/NadekoBot-BashScript/raw/1.4/nadeko_installer_latest.sh && bash "$root/nadeko_installer_latest.sh"; sleep 5s; done
+while :; do cd "$root/MidnightBot" && dotnet restore && dotnet build --configuration Release && cd "$root/MidnightBot/src/NadekoBot" && dotnet run -c Release && cd "$root" && wget -N https://github.com/Midnight-Myth/NadekoBot-BashScript/raw/1.4/nadeko_installer_latest.sh && bash "$root/nadeko_installer_latest.sh"; sleep 5s; done
 echo ""
 echo "That didn't work? Please report in #NadekoLog Discord Server."
 sleep 3s
