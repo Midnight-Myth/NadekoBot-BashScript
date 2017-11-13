a57d70bff3660f318254dc2ffc7930ea707986a4
Midnight-Myth <Midnight-Myth@users.noreply.github.com>
2017-11-13 18:42:11 +0100

    Update linuxAIO.sh

--- linuxAIO.sh
+++ linuxAIO.sh
@@ -1,65 +1,168 @@
 #!/bin/sh
-echo ""
-echo "Welcome to NadekoBot."
 
-choice=6
-	echo "1. Download Dev Build (latest)"
-	echo "2. Download Stable Build"
-	echo "3. Run Nadeko (normally)"
-	echo "4. Run Nadeko with Auto Restart (check if nadeko is working properly, before using this)"
-	echo "5. To exit"
-	echo -n "Choose [1 or 2] to Download [3 or 4] to Run."
-while [ $choice -eq 6 ]; do
+echo "Welcome to MidnightBot."
+root=$(pwd)
+echo ""
+choice=7
+	echo "1. Download MidnightBot"
+	echo "2. Run Midnight (Normally)"
+	echo "3. Run Midnight with Auto Restart"
+	echo "4. Auto-Install Prerequisites (For Ubuntu, Debian and CentOS)"
+	echo "5. Set up credentials.json (If you have downloaded MidnightBot already)"
+	echo "6. Exit"
+	echo -n "Choose [1] to Download, [2 or 3] to Run, or [6] to Exit"
+while [ $choice -eq 7 ]; do
 read choice
 if [ $choice -eq 1 ] ; then
 
 	echo ""
-	echo "Downloading NadekoBot, please wait."
-	curl -L https://github.com/Midnight-Myth/NadekoBot-BashScript/raw/master/nadeko_installer_latest.sh | sh
+	echo "Downloading MidnightBot, please wait."
+	wget -N https://github.com/Midnight-Myth/NadekoBot-BashScript/raw/2.0/midnight_installer_latest.sh && bash "$root/midnight_installer_latest.sh"
 	echo ""
-	echo "NadekoBot Dev Build (latest) downloaded."
-	bash linuxAIO.sh
+	bash "$root/linuxAIO.sh"
 else
-	if [ $choice -eq 2 ] ; then
-		echo ""
-		echo "Downloading NadekoBot, please wait."
-		curl -L https://github.com/Midnight-Myth/NadekoBot-BashScript/raw/master/nadeko_installer.sh | sh
-		echo ""
-		echo "NadekoBot Stable Build downloaded."
-		bash linuxAIO.sh
-	else
-		if [ $choice -eq 3 ] ; then
+		if [ $choice -eq 2 ] ; then
 			echo ""
-			echo "Running Nadeko Normally, if you are running this to check Nadeko, use .die command on discord to stop Nadeko."
-			curl -L https://github.com/Midnight-Myth/NadekoBot-BashScript/raw/master/nadeko_run.sh | sh
+			echo "Running Midnight Normally, if you are running this to check Midnight, use .die command on discord to stop Midnight."
+			wget -N https://github.com/Midnight-Myth/NadekoBot-BashScript/raw/2.0/midnight_run.sh && bash "$root/midnight_run.sh"
 			echo ""
-			echo "Welcome back to NadekoBot."
-			bash linuxAIO.sh
+			echo "Welcome back to MidnightBot."
+			sleep 2s
+			bash "$root/linuxAIO.sh"
 		else
-			if [ $choice -eq 4 ] ; then
+			if [ $choice -eq 3 ] ; then
 				echo ""
-				echo "Running Nadeko with Auto Restart you will have to close the session to stop the auto restart."
+				echo "Running Midnight with Auto Restart you will have to close the session to stop the auto restart."
 				sleep 5s
-				wget -N https://github.com/Midnight-Myth/NadekoBot-BashScript/raw/master/NadekoAutoRestartAndUpdate.sh && bash NadekoAutoRestartAndUpdate.sh
+				wget -N https://github.com/Midnight-Myth/NadekoBot-BashScript/raw/2.0/MidnightAutoRestartAndUpdate.sh && bash "$root/MidnightAutoRestartAndUpdate.sh"
 				echo ""
-				echo "Looks like that didn't work."
+				echo "That did not work?"
+				sleep 2s
+				bash "$root/linuxAIO.sh"
 			else
-				if [ $choice -eq 5 ] ; then
+				if [ $choice -eq 4 ] ; then
 					echo ""
-					echo "Exiting..."
-					exit 0
+					echo "Getting the Auto-Installer for Debian/Ubuntu"
+					wget -N https://github.com/Midnight-Myth/NadekoBot-BashScript/raw/2.0/midnightautoinstaller.sh && bash "$root/midnightautoinstaller.sh"
+					echo ""
+					echo "Welcome back..."
+					sleep 2s
+					bash "$root/linuxAIO.sh"
 				else
-					echo "1. Download Dev Build (latest)"
-					echo "2. Download Stable Build"
-					echo "3. Run Nadeko (normally)"
-					echo "4. Run Nadeko with Auto Restart (check if nadeko is working properly, before using this)"
-					echo "5. To exit"
-					echo -n "Choose [1 or 2] to Download [3 or 4] to Run."
-					choice=6
+					if [ $choice -eq 5 ] ; then
+						echo ""
+						echo
+echo -e "Let's begin creating a new credentials.json file if you are about to run the MidnightBot for the first time. \n \nPlease read JSON Explanations in the guide... \n \nPress [Y] when you are ready to continue or [N] to exit."
+while true; do
+    read -p "[y/n]: " yn
+    case $yn in
+        [Yy]* ) clear; break;;
+        [Nn]* ) echo Exiting...; exit;;
+        * ) echo "Couldn't get that please type [y] for Yes or [n] for No.";;
+    esac
+done
+clear
+cd "$root/MidnightBot/src/MidnightBot"
+mv credentials.json credentials.json.old
+
+echo Please enter your bot client ID:
+read clientid
+echo Alright saved \'$clientid\' as your client ID.
+echo ""
+echo "----------"
+echo ""
+
+echo Please enter your bot token \(It is not bot secret, it should be ~59 characters long.\):
+read token
+echo Alright saved \'$token\' as your bot\'s token.
+echo ""
+echo "----------"
+echo ""
+
+echo Please enter your own ID \(Refer to the guide, it will be bot\'s owner ID.\):
+read ownerid
+echo Alright saved \'$ownerid\' as owner\'s ID.
+echo ""
+echo "----------"
+echo ""
+
+echo Please enter Google API key \(Refer to the guide.\):
+read googleapi
+echo Alright saved \'$googleapi\' as your bot\'s Google API Key.
+echo ""
+echo "----------"
+echo ""
+
+echo Please enter LoL API Key or Just Press [Enter Key] to skip. \(optional\) \(Refer to the JSON Explanations guide.\):
+read lolapikey
+echo Alright saved \'$lolapikey\'
+echo ""
+echo "----------"
+echo ""
+
+echo Please enter Mashape Key or Just Press [Enter Key] to skip. \(optional\) \(Refer to the JSON Explanations guide.\):
+read mashapekey
+echo Alright saved \'$mashapekey\'
+echo ""
+echo "----------"
+echo ""
+
+echo Please enter Osu API Key or Just Press [Enter Key] to skip. \(optional\) \(Refer to the JSON Explanations guide.\):
+read osu
+echo Alright saved \'$osu\'
+echo ""
+echo "----------"
+echo ""
+
+echo Please enter Cleverbot Api Key or Just Press [Enter Key] to skip. \(optional\) \(Refer to the JSON Explanations guide.\):
+read cleverbot
+echo Alright saved \'$cleverbot\'
+echo ""
+echo "----------"
+echo ""
+
+echo "{
+  \"ClientId\": $clientid,
+  \"Token\": \"$token\",
+  \"OwnerIds\": [
+    $ownerid
+  ],
+  \"LoLApiKey\": \"$lolapikey\",
+  \"GoogleApiKey\": \"$googleapi\",
+  \"MashapeKey\": \"$mashapekey\",
+  \"OsuApiKey\": \"$osu\",
+  \"CleverbotApiKey\": \"$cleverbot\",
+  \"Db\": null,
+  \"TotalShards\": 1
+}" | cat - >> credentials.json
+echo Credentials setup completed.
+sleep 5
+clear
+cd "$root"
+bash "$root/linuxAIO.sh"
+					else
+						if [ $choice -eq 6 ] ; then
+							echo ""
+							echo "Exiting..."
+							cd "$root"
+							exit 0
+						else
+							clear
+							echo "1. Download MidnightBot"
+							echo "2. Run Midnight (Normally)"
+							echo "3. Run Midnight with Auto Restart"
+							echo "4. Auto-Install Prerequisites (For Ubuntu, Debian and CentOS)"
+							echo "5. Set up credentials.json (If you have downloaded MidnightBot already)"
+							echo "6. Exit"
+							echo -n "Choose [1] to Download, [2 or 3] to Run, or [6] to Exit."
+							choice=7
+						fi
+					fi
 				fi
 			fi
 		fi
 	fi
-fi
 done
+
+cd "$root"
 exit 0
