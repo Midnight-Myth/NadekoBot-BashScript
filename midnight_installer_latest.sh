b91a3816b34b0279859e929bd3abc1ce2a903d49
Midnight-Myth <Midnight-Myth@users.noreply.github.com>
2017-11-13 19:35:37 +0100

    Update midnight_installer_latest.sh

--- midnight_installer_latest.sh
+++ midnight_installer_latest.sh
@@ -58,17 +58,17 @@ else
     rm -rf MidnightBot_old 1>/dev/null 2>&1
     mv -fT MidnightBot MidnightBot_old 1>/dev/null 2>&1
     mv "$tempdir"/MidnightBot MidnightBot
-    cp -f "$root/MidnightBot_old/src/MidnightBot/credentials.json" "$root/MidnightBot/src/MidnightBot/credentials.json" 1>/dev/null 2>&1
+    cp -f "$root/MidnightBot_old/src/NadekoBot/credentials.json" "$root/MidnightBot/src/NadekoBot/credentials.json" 1>/dev/null 2>&1
     echo ""
     echo "credentials.json copied to the new version"
-    cp -RT "$root/MidnightBot_old/src/MidnightBot/bin/" "$root/MidnightBot/src/MidnightBot/bin/" 1>/dev/null 2>&1
-    cp -RT "$root/MidnightBot/src/MidnightBot/bin/Release/netcoreapp1.0/data/MidnightBot.db" "$root/MidnightBot/src/MidnightBot/bin/Release/netcoreapp2.0/data/MidnightBot.db" 1>/dev/null 2>&1
-	cp -RT "$root/MidnightBot/src/MidnightBot/bin/Release/netcoreapp1.1/data/MidnightBot.db" "$root/MidnightBot/src/MidnightBot/bin/Release/netcoreapp2.0/data/MidnightBot.db" 1>/dev/null 2>&1
-    mv -f "$root/MidnightBot/src/MidnightBot/bin/Release/netcoreapp1.0/data/MidnightBot.db" "$root/MidnightBot/src/MidnightBot/bin/Release/netcoreapp1.0/data/MidnightBot_old.db" 1>/dev/null 2>&1
-	mv -f "$root/MidnightBot/src/MidnightBot/bin/Release/netcoreapp1.1/data/MidnightBot.db" "$root/MidnightBot/src/MidnightBot/bin/Release/netcoreapp1.1/data/MidnightBot_old.db" 1>/dev/null 2>&1
+    cp -RT "$root/MidnightBot_old/src/NadekoBot/bin/" "$root/MidnightBot/src/MidnightBot/bin/" 1>/dev/null 2>&1
+    cp -RT "$root/MidnightBot/src/NadekoBot/bin/Release/netcoreapp1.0/data/MidnightBot.db" "$root/MidnightBot/src/NadekoBot/bin/Release/netcoreapp2.0/data/MidnightBot.db" 1>/dev/null 2>&1
+	cp -RT "$root/MidnightBot/src/NadekoBot/bin/Release/netcoreapp1.1/data/MidnightBot.db" "$root/MidnightBot/src/NadekoBot/bin/Release/netcoreapp2.0/data/MidnightBot.db" 1>/dev/null 2>&1
+    mv -f "$root/MidnightBot/src/NadekoBot/bin/Release/netcoreapp1.0/data/MidnightBot.db" "$root/MidnightBot/src/NadekoBot/bin/Release/netcoreapp1.0/data/MidnightBot_old.db" 1>/dev/null 2>&1
+	mv -f "$root/MidnightBot/src/NadekoBot/bin/Release/netcoreapp1.1/data/MidnightBot.db" "$root/MidnightBot/src/NadekoBot/bin/Release/netcoreapp1.1/data/MidnightBot_old.db" 1>/dev/null 2>&1
     echo ""
     echo "Database copied to the new version"
-    cp -RT "$root/MidnightBot_old/src/MidnightBot/data/" "$root/MidnightBot/src/MidnightBot/data/" 1>/dev/null 2>&1
+    cp -RT "$root/MidnightBot_old/src/NadekoBot/data/" "$root/MidnightBot/src/NadekoBot/data/" 1>/dev/null 2>&1
     echo ""
     echo "Other data copied to the new version"
 fi
