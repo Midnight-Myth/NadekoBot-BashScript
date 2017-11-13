7642f8e431ca073b467f0d67f2889935c2dfbda2
Midnight-Myth <Midnight-Myth@users.noreply.github.com>
2017-11-13 18:33:51 +0100

    Update and rename NadekoAutoRestartAndUpdate.sh to MidnightAutoRestartAndUpdate.sh

---
+++ MidnightAutoRestartAndUpdate.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+echo ""
+echo "Welcome to MidnightBot Auto Restart and Update!"
+echo ""
+root=$(pwd)
+
+
+choice=4
+	echo "1. Run Auto Restart normally without updating MidnightBot."
+	echo "2. Run Auto Restart and update MidnightBot."
+	echo "3. Exit"
+	echo -n "Choose [1] to Run MidnightBot with auto restart on "die" command without updating itself, [2] to Run with Auto Updating on restart after using "die" command."
+while [ $choice -eq 4 ]; do
+read choice
+if [ $choice -eq 1 ] ; then
+	echo ""
+	wget -N https://github.com/Midnight-Myth/NadekoBot-BashScript/raw/2.0/MidnightARN.sh && bash "$root/MidnightARN.sh"
+else
+	if [ $choice -eq 2 ] ; then
+		echo ""
+		wget -N https://github.com/Midnight-Myth/NadekoBot-BashScript/raw/2.0/MidnightARU_Latest.sh && bash "$root/MidnightARU_Latest.sh"
+	else
+			if [ $choice -eq 3 ] ; then
+				echo ""
+				echo "Exiting..."
+				cd "$root"
+				exit 0
+			else
+				clear
+				echo "1. Auto Restart normally without updating MidnightBot."
+				echo "2. Auto Restart and update MidnightBot."
+				echo "3. Exit"
+				echo -n "Choose [1] to Run MidnightBot with auto restart on "die" command without updating itself, [2] to Run with Auto Updating on restart after using "die" command."
+				choice=4
+			fi
+		
+	fi
+fi
+done
+
+cd "$root"
+rm "$root/MidnightAutoRestartAndUpdate.sh"
+exit 0
